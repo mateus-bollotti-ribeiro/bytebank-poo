56f089da363a180afe66fdcfed275ade33828d8e
Mateus Ribeiro <mateus.ribeiro.dev@gmail.com>
2022-08-07 20:51:16 -0300

    Diversas remoções, adições e melhorias na classe "Cliente"

--- Cliente.java
+++ Cliente.java
@@ -4,28 +4,40 @@ public class Cliente {
     private String nome;
     private String cpf;
     private String profissao;
+    private String sexo;
+    private String estadoCivil;
+    private static int total;
+
+    public Cliente(String nome, String cpf, String profissao, String sexo, String estadoCivil) {
+        Cliente.total++;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.profissao = profissao;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+
+        System.out.println(this.nome + this.cpf + this.profissao + this.sexo + this.estadoCivil);
+        System.out.println("O numero total de Clientes cadastrados no momento é de: " + Cliente.total);
+        System.out.println(" ");
+    }
 
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getProfissao() {
         return profissao;
     }
 
-    public void setProfissao(String profissao) {
-        this.profissao = profissao;
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
     }
 }
